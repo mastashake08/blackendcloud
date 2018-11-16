5d48c4e57a28ed8b2072f0958e1fd5bd36217560
Jyrone Parker <jyrone.parker@gmail.com>
2018-11-16 16:33:39 -0500

    verified middlware

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');;
 Route::get('/pricing','HomeController@pricing')->name('pricing');
 Route::get('/features','HomeController@features')->name('features');
 Route::get('/contact','HomeController@contact')->name('contact');
